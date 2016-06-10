0b4768e015bf41ef50105e3b9f1b82ebed7c150f
Vitaly Orlov <vitaliuorlov@gmail.com>
2016-06-10 14:02:33 +0300

    add update option

--- handler.sh
+++ handler.sh
@@ -3,18 +3,49 @@
 START_SCRIPT="servantjs-server.js"
 INSTALL_DIR="/usr/local/servantjs/server"
 
-cd ${INSTALL_DIR}
+SERVANT_DIR="/usr/local/servantjs"
+INSTALL_DIR="${SERVANT_DIR}/server"
+GIT_NAME="${SERVANT_DIR}/servantjs"
+LINK_PATH="/usr/local/bin/servant-server"
+BACKUP_DIR="${SERVANT_DIR}/.server_backup"
+INSTALL_BACKUP_FILE="${SERVANT_DIR}/.server_install.sh"
 
 function start {
+    cd ${INSTALL_DIR}
     forever start ${START_SCRIPT}
 }
 
 function stop {
+    cd ${INSTALL_DIR}
     forever stop ${START_SCRIPT}
 }
 
 function status {
+    cd ${INSTALL_DIR}
     forever list | grep "pid\|${START_SCRIPT}"
+}
+
+function update {
+    cp -r ${INSTALL_DIR} ${BACKUP_DIR}
+    rm -f ${LINK_PATH}
+
+    cp ${INSTALL_DIR}/install.sh ${INSTALL_BACKUP_FILE}
+
+    rm -fr ${INSTALL_DIR}
+
+    bash ${INSTALL_BACKUP_FILE}
+
+    if [ $? != 0 ]; then
+        rm -fr ${GIT_NAME}
+        rm -fr ${INSTALL_DIR}
+        ln -s ${INSTALL_DIR}/handler.sh ${LINK_PATH}
+        mv ${BACKUP_DIR} ${INSTALL_DIR}
+    else
+        cp ${BACKUP_DIR}/config.json ${INSTALL_DIR}/
+        rm -fr ${BACKUP_DIR}
+    fi
+
+    rm -f ${INSTALL_BACKUP_FILE}
 }
 
 case "$1" in
@@ -31,8 +62,11 @@ case "$1" in
   status)
     status
     ;;
+  update)
+    update
+    ;;
   *)
-    echo "Usage: servant-server <start|stop|restart|status>"
+    echo "Usage: servant-server <start|stop|restart|status|update>"
     exit 1
     ;;
 
